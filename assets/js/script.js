e1b439d977688668c56b8f18b8b4cfcf1cd9204a
tmunkhb <tmunkhb@gmail.com>
2022-09-18 20:35:15 -0400

    finished html, added js vars

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//variables
+let city="";
+let searchCity = $("#search-city");
+let searchButton = $("#search-button");
+let clearButton = $("#clear-history");
+let currentCity = $("#current-city");
+let currentTemperature = $("#temperature");
+let currentHumidty= $("#humidity");
+let currentWSpeed=$("#wind-speed");
+let currentUvindex= $("#uv-index");
+let sCity=[];
+
+//API key variable
+const APIkey = "78b9ad515dca05a6cf7e5d876a98f14b";
